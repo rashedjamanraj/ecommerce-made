bc7b770b390963c6e115c44423de63a0531eba9f
rashedjamanraj <rashed.jaman.raj@gmail.com>
2026-05-08 18:20:16 +0600

    mobile menu cart

--- src/components/navbar/shared/MobileiiMenu.tsx
+++ src/components/navbar/shared/MobileiiMenu.tsx
@@ -4,21 +4,44 @@
 
 import React, { useState } from 'react'
 import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai'
+import { useCart } from './CartContext';
+import { HiShoppingCart } from 'react-icons/hi2';
+import CartList from './CartList';
 
 const MobileMenu = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [activePanel, setActivePanel] = useState("");
+  const {cart} = useCart();
   const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen)
+    setIsMenuOpen((prev) => !prev);
   }
 
 
   return (
-    <div className=" lg:hidden">
-          <div onClick={toggleMenu} >
+    <div className=" lg:hidden flex items-center gap-4">
+      {/* Hamburger toggle */}
+          <div onClick={toggleMenu} className='cursor-pointer'>
             {
               isMenuOpen ? <AiOutlineClose size={24} /> : <AiOutlineMenu size={24}  />
             }
           </div>
+
+          {/* Cart button */}
+          <button onClick={() => setActivePanel("cartlist")}>
+            <HiShoppingCart size={26}/>
+            { cart.length > 0 && (
+              <span className="absolute top-4 right-2 bg-rose-600 text-white text-xs rounded-full h-5 w-5 flex items-center justify-center">
+                {cart.length}
+              </span>
+            )}
+          </button>
+
+          {/* Slide-in Cart Panel */}
+          <CartList 
+            activePanel={activePanel}
+            handleClose={() => setActivePanel("")}
+            setOrderSummary={(val) => console.log(`Checkout: ${val}`)}
+          />
         </div>
   )
 }
