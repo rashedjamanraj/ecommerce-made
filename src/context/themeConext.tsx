879a2618767ef7c09a27ce4865c6004fefb29009
Rashed Jaman Raj <rashed.jaman.raj@gmail.com>
2026-05-12 10:34:04 +0600

    Add useTheme hook for accessing theme context

--- src/context/themeConext.tsx
+++ src/context/themeConext.tsx
@@ -37,3 +37,12 @@ export const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
     </ThemeContext.Provider>
   );
 };
+
+
+export const useTheme = () => {
+  const context = useContext(ThemeContext);
+  if (!context) {
+    throw new Error("useTheme must be used within a ThemeProvider");
+  }
+  return context;
+};
